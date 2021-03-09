9590d16908c3a4583cad1a773aa45356dd9380b2
deen13 <dierkes@smartsquare.de>
2021-03-09 14:13:39 +0100

    Fix disconnect issue

--- src/main/kotlin/de/smartsquare/smartbot/starter/mqtt/Adapter.kt
+++ src/main/kotlin/de/smartsquare/smartbot/starter/mqtt/Adapter.kt
@@ -2,15 +2,20 @@ package de.smartsquare.smartbot.starter.mqtt
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.hivemq.client.mqtt.mqtt3.Mqtt3Client
+import com.hivemq.client.mqtt.mqtt3.message.publish.Mqtt3Publish
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.stereotype.Component
+import java.lang.reflect.Method
 
 @Component
 class Adapter(
-  private val collector: AnnotationCollector,
-  private val jackson: ObjectMapper,
-  private val client: Mqtt3Client
+    private val collector: AnnotationCollector,
+    private val jackson: ObjectMapper,
+    private val client: Mqtt3Client
 ) : InitializingBean {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     private val asyncClient = client.toAsync()
 
@@ -24,9 +29,17 @@ class Adapter(
                 asyncClient.subscribeWith()
                     .topicFilter(topic)
                     .qos(annotation.qos)
-                    .callback { msg -> subscriber.invoke(bean, jackson.readValue(msg.payloadAsBytes, payloadType)) }
+                    .callback { msg -> deliver(subscriber, bean, msg, payloadType) }
                     .send()
             }
+        }
+    }
+
+    private fun deliver(subscriber: Method, bean: Any, msg: Mqtt3Publish, payloadType: Class<*>) {
+        try {
+            subscriber.invoke(bean, jackson.readValue(msg.payloadAsBytes, payloadType))
+        } catch (e: Exception) {
+            logger.error("Error while delivering the message.", e)
         }
     }
 }
