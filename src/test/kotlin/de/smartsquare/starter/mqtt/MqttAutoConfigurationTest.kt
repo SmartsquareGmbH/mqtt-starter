f6fe96e08ecb1e3c6849461d8bcbed71e2aed57c
Ruben Gees <rubengees7@googlemail.com>
2021-10-19 12:26:43 +0200

    Make test typesafe

--- src/test/kotlin/de/smartsquare/starter/mqtt/MqttAutoConfigurationTest.kt
+++ src/test/kotlin/de/smartsquare/starter/mqtt/MqttAutoConfigurationTest.kt
@@ -1,7 +1,11 @@
 package de.smartsquare.starter.mqtt
 
-import org.amshove.kluent.shouldBeFalse
+import org.amshove.kluent.invoking
+import org.amshove.kluent.shouldThrow
+import org.amshove.kluent.withMessage
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.NoSuchBeanDefinitionException
+import org.springframework.beans.factory.getBean
 import org.springframework.boot.context.annotation.UserConfigurations
 import org.springframework.boot.test.context.runner.ApplicationContextRunner
 
@@ -13,6 +17,10 @@ class MqttAutoConfigurationTest {
     @Test
     fun `should not be loaded if mqtt enabled is set to false`() {
         runner.withPropertyValues("mqtt.enabled=false")
-            .run { context -> context.containsBean("mqttAutoConfiguration").shouldBeFalse() }
+            .run { context ->
+                invoking { context.getBean<MqttAutoConfiguration>() } shouldThrow
+                    NoSuchBeanDefinitionException::class withMessage
+                    "No qualifying bean of type 'de.smartsquare.starter.mqtt.MqttAutoConfiguration' available"
+            }
     }
 }
