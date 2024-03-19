2b86de0af1f3de0d3e892597c4d7656b999e606a
Ruben Gees <rubengees7@gmail.com>
2024-03-19 09:47:59 +0100

    Add shutdown test

--- src/test/kotlin/de/smartsquare/starter/mqtt/MqttAutoConfigurationTest.kt
+++ src/test/kotlin/de/smartsquare/starter/mqtt/MqttAutoConfigurationTest.kt
@@ -1,14 +1,19 @@
 package de.smartsquare.starter.mqtt
 
+import com.hivemq.client.mqtt.mqtt5.Mqtt5Client
 import org.amshove.kluent.invoking
+import org.amshove.kluent.shouldBeFalse
+import org.amshove.kluent.shouldBeTrue
 import org.amshove.kluent.shouldThrow
 import org.amshove.kluent.withMessage
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.NoSuchBeanDefinitionException
 import org.springframework.beans.factory.getBean
 import org.springframework.boot.context.annotation.UserConfigurations
 import org.springframework.boot.test.context.runner.ApplicationContextRunner
 
+@ExtendWith(EmqxExtension::class)
 class MqttAutoConfigurationTest {
 
     private val runner = ApplicationContextRunner()
@@ -22,5 +27,18 @@ class MqttAutoConfigurationTest {
                     NoSuchBeanDefinitionException::class withMessage
                     "No qualifying bean of type 'de.smartsquare.starter.mqtt.MqttAutoConfiguration' available"
             }
+    }
+
+    @Test
+    fun `should shutdown cleanly`() {
+        runner.withPropertyValues("mqtt.version=5").run { context ->
+            val client = context.getBean<Mqtt5Client>()
+
+            client.state.isConnected.shouldBeTrue()
+
+            context.stop()
+
+            client.state.isConnectedOrReconnect.shouldBeFalse()
+        }
     }
 }
