3bc7b061e8b15bc64d91698803472b5b77182534
cmdjulian <julian.goede@pm.me>
2024-04-16 22:58:36 +0200

    MR comments

--- src/test/kotlin/de/smartsquare/starter/mqtt/TestObjectProvider.kt
+++ src/test/kotlin/de/smartsquare/starter/mqtt/TestObjectProvider.kt
@@ -1,11 +1,10 @@
 package de.smartsquare.starter.mqtt
 
-import org.springframework.beans.factory.NoSuchBeanDefinitionException
 import org.springframework.beans.factory.ObjectProvider
 
-class TestObjectProvider<T : Any>(private val data: T?) : ObjectProvider<T> {
-    override fun getObject(vararg args: Any?): T = data ?: throw NoSuchBeanDefinitionException("No bean found")
-    override fun getObject(): T = data ?: throw NoSuchBeanDefinitionException("No bean found")
-    override fun getIfAvailable(): T? = data
+class TestObjectProvider<T : Any>(private val data: T) : ObjectProvider<T> {
+    override fun getObject(vararg args: Any?): T = data
+    override fun getObject(): T = data
+    override fun getIfAvailable(): T = data
     override fun getIfUnique(): T = getObject()
 }
